a0cc1e8b5966751306781ceef7efaca7764aa423
Arttx007 <s.arthur.ricardo@gmail.com>
2025-10-13 23:57:55 -0300

    Delete porAmorDeMuitos-main/porAmorDeMuitos-main/poramordemuitos directory

--- porAmorDeMuitos-main/porAmorDeMuitos-main/poramordemuitos/src/main/java/com/poramordemuitos/repository/BeneficiarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.poramordemuitos.repository;
-
-import com.poramordemuitos.model.Beneficiario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BeneficiarioRepository extends JpaRepository<Beneficiario, Long> {}
--- porAmorDeMuitos-main/porAmorDeMuitos-main/poramordemuitos/src/main/java/com/poramordemuitos/repository/PermissaoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.poramordemuitos.repository;
-
-import com.poramordemuitos.model.Permissao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissaoRepository extends JpaRepository<Permissao, Long> {}
